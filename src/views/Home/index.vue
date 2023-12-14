54542bb3100ce2c7b361c7d3091076cae28bf539
wangshuo7 <13665439520@163.com>
2023-12-14 11:12:36 +0800

    fix：修复首次进入bug

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -9,6 +9,15 @@ export default {
   name: 'Home'
 }
 </script>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+import { getPersonalInfo } from '../../api/personal'
+async function query() {
+  await getPersonalInfo()
+}
+onMounted(() => {
+  query()
+})
+</script>
 
 <style lang="less" scoped></style>
